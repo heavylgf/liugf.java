b3cce6367bcf1a5d2dc1cd779c4434960e81f720
liugf <863065649@qq.com>
2020-08-26 20:35:54 +0800

    jvm参数

---
+++ javabase-demo/src/main/java/com/zhss/javabase/jvm/Demo1.java
@@ -0,0 +1,20 @@
+package com.zhss.javabase.jvm;
+
+/**
+ * JVM实战
+ * @Author: liugf
+ * @Date: 2020/8/26 下午5:40
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        byte[] array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+
+    }
+
+}
